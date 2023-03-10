5602fea7c7743cb73bb4f0906a515e6f0af1dae2
Vera-Miyazaki <s50797@edu.campus02.at>
2023-03-10 17:08:55 +0100

    Initial commit

---
+++ src/main/java/com/KiWi/WebPortal/WebPortalApplication.java
@@ -0,0 +1,13 @@
+package com.KiWi.WebPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPortalApplication.class, args);
+	}
+
+}
